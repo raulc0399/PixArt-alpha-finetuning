a3d4d0798c484c69bf3eb96fe6fb1efdefa46659
raulc <raulciotescu@gmail.com>
2024-04-05 22:03:08 +0200

    moved datasets to new folder, to have them as oxen repo

--- prepare/01_convert.py
+++ prepare/01_convert.py
@@ -7,14 +7,14 @@ def save_to_jsonl(df, file_path):
 
 this_dir = os.path.dirname(__file__)
 
-datasets_dir = os.path.join(this_dir, "../data/datasets/")
+datasets_dir = os.path.join(this_dir, "../../data/input/")
 file1_path = os.path.join(datasets_dir, "1.parquet")
 file2_path = os.path.join(datasets_dir, "2.parquet")
 
 df1 = pd.read_parquet(file1_path)
 df2 = pd.read_parquet(file2_path)
 
-output_folder_path = os.path.join(this_dir, "../data/train/")
+output_folder_path = os.path.join(this_dir, "../../data/train/")
 metadata_file_path = os.path.join(output_folder_path, "metadata.jsonl")
 
 # Merge the two dataframes and save the as HF imagefolder - in fine-tuning, only one text column is needed, but we will save also the text from recaptioning
