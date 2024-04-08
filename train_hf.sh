59e3ead8bb224c1b19195cdacf1cc4cd10fb15b9
raulc <raulciotescu@gmail.com>
2024-04-08 21:49:42 +0200

    training params

--- train_hf.sh
+++ train_hf.sh
@@ -13,12 +13,14 @@ accelerate launch --num_processes=1 --main_process_port=36667 PixArt-alpha/train
   --pretrained_model_name_or_path=PixArt-alpha/PixArt-XL-2-512x512 \
   --train_data_dir="../data/train/" --caption_column="llava_caption_with_orig_caption" \
   --resolution=512 --random_flip \
-  --train_batch_size=16 \
-  --num_train_epochs=200 --checkpointing_steps=100 \
+  --train_batch_size=4 --gradient_accumulation_steps=4 \
+  --num_train_epochs=4 --checkpointing_steps=10 \
   --learning_rate=1e-06 --lr_scheduler="constant" --lr_warmup_steps=0 \
   --seed=42 \
   --output_dir="pixart-simpson-model" \
-  --validation_prompt="cute dragon creature" --report_to="tensorboard" \
-  --gradient_checkpointing --checkpoints_total_limit=10 --validation_epochs=5 \
-  --rank=16 \
-  --use_8bit_adam
+  --report_to="tensorboard" \
+  --gradient_checkpointing --checkpoints_total_limit=10 \
+  --rank=16
+
+# could be used if enough GPU memory
+  # --validation_prompt="cute dragon creature" --validation_epochs=2 \
