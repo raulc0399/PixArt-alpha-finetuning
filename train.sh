df3bec76bd19d4eec472c63970f186233d5b4d3d
raulc <raulciotescu@gmail.com>
2024-04-08 19:58:44 +0200

    training script

---
+++ train.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# clone original repo and then run the training script
+# git clone https://github.com/PixArt-alpha/PixArt-alpha.git
+
+accelerate launch --num_processes=1 --main_process_port=36667 PixArt-alpha/train_scripts/train_pixart_lora_hf.py --mixed_precision="fp16" \
+  --pretrained_model_name_or_path=PixArt-alpha/PixArt-XL-2-512x512 \
+  --train_dir="../data/train/" --caption_column="llava_caption_with_orig_caption" \
+  --resolution=512 --random_flip \
+  --train_batch_size=16 \
+  --num_train_epochs=200 --checkpointing_steps=100 \
+  --learning_rate=1e-06 --lr_scheduler="constant" --lr_warmup_steps=0 \
+  --seed=42 \
+  --output_dir="pixart-pokemon-model" \
+  --validation_prompt="cute dragon creature" --report_to="tensorboard" \
+  --gradient_checkpointing --checkpoints_total_limit=10 --validation_epochs=5 \
+  --rank=16 \
+  --use_8bit_adam
