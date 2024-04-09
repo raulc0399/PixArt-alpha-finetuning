3e82a4751f9f88fa83c4cb56a4327a8763970767
raulc <raulciotescu@gmail.com>
2024-04-09 23:42:03 +0200

    75 epochs

--- train_hf.sh
+++ train_hf.sh
@@ -17,17 +17,15 @@
 
 # remove the validation_epochs or set it to a lower number if you want to run the validation prompt
 # validation will be ran at the end
-current_time=$(date +'%Y%m%d-%H%M%S')
 accelerate launch --num_processes=1 --main_process_port=36667 PixArt-alpha/train_scripts/train_pixart_lora_hf.py --mixed_precision="fp16" \
   --pretrained_model_name_or_path=PixArt-alpha/PixArt-XL-2-512x512 \
   --train_data_dir="../data/train/" --caption_column="orig_text" \
   --resolution=512 --random_flip \
   --train_batch_size=2 --gradient_accumulation_steps=4 \
-  --num_train_epochs=50 --checkpointing_steps=25 \
+  --num_train_epochs=75 --checkpointing_steps=25 \
   --learning_rate=1e-06 --lr_scheduler="constant" --lr_warmup_steps=50 \
   --seed=42 \
-  --output_dir="pixart-simpson-model/$current_time" \
-  --logging_dir="pixart-simpson-model/log" \
+  --output_dir="pixart-simpson-model" \
   --report_to="tensorboard" \
   --gradient_checkpointing --checkpoints_total_limit=10 \
   --validation_epochs=1000 \
@@ -61,4 +59,4 @@ accelerate launch --num_processes=1 --main_process_port=36667 PixArt-alpha/train
 # 2. 1e-06, linear
 # 3. 1e-05, constant https://keras.io/examples/generative/finetune_stable_diffusion/#initialize-the-trainer-and-compile-it
 # 4. 1e-05, cosine
-# 5. 1e-06, constant, num_train_epochs=50, lr_warmup_steps=50, train_batch_size=2, 8bit adam, orig_text
+# 5. 1e-06, constant, num_train_epochs=75, lr_warmup_steps=50, train_batch_size=2, 8bit adam, orig_text
