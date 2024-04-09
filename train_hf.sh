a3ce61edea41815152a00b2e4d0b1b1651df5d86
raulc <raulciotescu@gmail.com>
2024-04-09 15:02:47 +0200

    lr_scheduler

--- train_hf.sh
+++ train_hf.sh
@@ -23,7 +23,7 @@ accelerate launch --num_processes=1 --main_process_port=36667 PixArt-alpha/train
   --resolution=512 --random_flip \
   --train_batch_size=4 --gradient_accumulation_steps=4 \
   --num_train_epochs=4 --checkpointing_steps=25 \
-  --learning_rate=1e-06 --lr_scheduler="constant" --lr_warmup_steps=0 \
+  --learning_rate=1e-06 --lr_scheduler="linear" --lr_warmup_steps=0 \
   --seed=42 \
   --output_dir="pixart-simpson-model" \
   --report_to="tensorboard" \
@@ -36,7 +36,7 @@ accelerate launch --num_processes=1 --main_process_port=36667 PixArt-alpha/train
 # lora layers
 # rank
 # learning rate
-# lr_scheduler
+# lr_scheduler: constant, linear
 # lr_warmup_steps
 # resolution
 # gradient clipping
