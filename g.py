90ffac776408670a3964618a1e0cc82b34e80190
raulc <raulciotescu@gmail.com>
2024-04-12 14:12:31 +0200

    gen test imgs

--- g.py
+++ g.py
@@ -23,18 +23,22 @@ MODEL_ID = "PixArt-alpha/PixArt-XL-2-512x512"
 # del transformer
 
 def get_default_pipeline():
-    pipe = PixArtAlphaPipeline.from_pretrained(MODEL_ID, torch_dtype=torch.float16)
-    pipe.to("cuda")
+    pipe = PixArtAlphaPipeline.from_pretrained(MODEL_ID)
+    pipe.to("cuda", dtype=torch.float16)
     return pipe
 
 def get_lora_pipeline():
     transformer = Transformer2DModel.from_pretrained(MODEL_ID, subfolder="transformer", torch_dtype=torch.float16)
-    transformer = PeftModel.from_pretrained(transformer, paths.get_peft_folder())
+    # transformer.to("cuda", dtype=torch.float16)
+    transformer = PeftModel.from_pretrained(transformer, "./pixart-simpsons-model")
+    # transformer.to("cuda", dtype=torch.float16)
+    
     pipe = PixArtAlphaPipeline.from_pretrained(MODEL_ID, transformer=transformer, torch_dtype=torch.float16)
     pipe.to("cuda")
+    # pipe.to("cuda", dtype=torch.float16)
 
-    del transformer
-    torch.cuda.empty_cache()
+    # del transformer
+    # torch.cuda.empty_cache()
 
     return pipe
 
@@ -44,16 +48,18 @@ def generate_images(pipe, prefix):
         # "cute dragon creature",
         # "A family of four, with two parents and two children, having a picnic in a park. The sun is shining brightly, and there's a picnic basket filled with food. The family is sitting on a checkered blanket, laughing and enjoying their time together.",
         # "A scientist working in a lab filled with futuristic gadgets and machines. The scientist is holding a test tube with a glowing liquid, and there's a robot assistant helping him in the background. The lab has large windows showing a cityscape outside.",
-        # "A group of friends at a bowling alley. They are taking turns bowling, with one person about to release the ball down the lane. There's excitement and competition in the air, and the scoreboard shows a close game.",
+        "A group of friends at a bowling alley. They are taking turns bowling, with one person about to release the ball down the lane. There's excitement and competition in the air, and the scoreboard shows a close game.",
         # "A classroom scene with a teacher and students. The teacher is standing in front of a blackboard, explaining a complex math problem, while the students are listening intently, some scratching their heads and others taking notes.",
         # "A musician playing a guitar on stage at a rock concert. The crowd is cheering and waving their hands in the air, with lights flashing and a disco ball spinning overhead. The musician is passionately performing a solo, lost in the music.",
-        # "cartoon-like illustration of a crowd of people at a concert"
-
-        "simpsons fishing"
+        # "cartoon-like illustration of a crowd of people at a concert",
 
         # from the training set:
         # "The image is a cartoon depiction of the Simpsons family, consisting of Homer, Marge, Bart, and Lisa. They are walking through a park, enjoying a day outdoors.",
         # "The image is a cartoon-style drawing of a man wearing a tie and holding a cup. He is standing in front of a suitcase, which is open and has papers inside. The man appears to be a character from the popular animated show \"The Simpsons.\" The scene is set in a dark room, with the man being the main focus of the image."
+        # "homer, from the simpsons",
+        # "simpsons fishing on the river",
+        # "simpsons family in a tv set"
+
     ]
 
     output_dir = "./generated/"
@@ -62,7 +68,7 @@ def generate_images(pipe, prefix):
 
     current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     for i, prompt in enumerate(prompts):
-        image = pipe(prompt, num_inference_steps=20).images[0]    
+        image = pipe(prompt, width=512, height=512, num_inference_steps=20).images[0]    
 
         file_name = os.path.join(output_dir, f"{current_time}_{prefix}_img_{i}")
 
@@ -83,5 +89,5 @@ if __name__ == "__main__":
         del pipe
         torch.cuda.empty_cache()
 
-    pipe = get_lora_pipeline()
-    generate_images(pipe, "lora")
+    # pipe = get_lora_pipeline()
+    # generate_images(pipe, "lora")
