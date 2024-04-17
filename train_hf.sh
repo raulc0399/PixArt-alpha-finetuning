9ccffc200da8d751c3f35017056afa5c7b2ea934
raulc <raulciotescu@gmail.com>
2024-04-17 17:44:52 +0200

    new caption column

--- train_hf.sh
+++ train_hf.sh
@@ -19,7 +19,7 @@
 # validation will be ran at the end
 accelerate launch --num_processes=1 --main_process_port=36667 PixArt-alpha/train_scripts/train_pixart_lora_hf.py --mixed_precision="fp16" \
   --pretrained_model_name_or_path=PixArt-alpha/PixArt-XL-2-512x512 \
-  --train_data_dir="../data/train/" --caption_column="orig_text" \
+  --train_data_dir="../data/train/" --caption_column="llava_caption_with_orig_caption" \
   --resolution=512 --random_flip \
   --train_batch_size=2 --gradient_accumulation_steps=4 \
   --num_train_epochs=100 --checkpointing_steps=100 \
