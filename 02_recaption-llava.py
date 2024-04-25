72767cdce8b98e5791284d528ae6857a372eecd5
raulc <raulciotescu@gmail.com>
2024-04-25 10:32:42 +0200

    recaption for style

--- 02_recaption-llava.py
+++ 02_recaption-llava.py
@@ -12,7 +12,10 @@ metadata_file_path = paths.get_metadata_file_path()
 
 # https://arxiv.org/pdf/2310.00426.pdf, Fig. 10
 prompt_for_caption = "Describe this image and its style in a very detailed manner"
-prompt_for_caption_with_caption = "Give the caption of this image '{caption}', describe this image and its style in a very detailed manner"
+prompt_for_caption_with_caption = """Give the caption of this image '{caption}', describe this image and its style in a very detailed manner,
+start all with 'Image in the style of simpsons cartoons, ', do not use the name simpsons in the description, do not use the name of the characters in the description.
+specify if the image contains one or more persons, animals, or objects, detail each of them, for persons and animals specify their emotions, actions, and interactions as well as if they are adults or children.
+"""
 
 def get_quantization_config():
     quantization_config = BitsAndBytesConfig(
@@ -53,7 +56,7 @@ def generate_text(model, processor, prompt, image):
     
     return processor.decode(output[0], skip_special_tokens=True)
 
-def save_generation_info(index, prompt1, prompt2, orig_caption, caption, caption_with_orig_caption, model_name):
+def save_generation_info(index, prompt1, prompt2, orig_caption, caption, caption_with_orig_caption, model_name, img_name):
     output_folder = "./check"
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -62,6 +65,7 @@ def save_generation_info(index, prompt1, prompt2, orig_caption, caption, caption
 
     with open(output_file_path, "w") as f:
         f.write(f"---------------------------: {index}\n")
+        f.write(f"Image: {img_name}\n\n")
         f.write(f"Original Caption: {orig_caption}\n\n")
 
         f.write(f"Prompt: {prompt1}\n")
@@ -98,7 +102,7 @@ def generate_images_captions(df, model, processor, model_prompt, model_eop, mode
         df.at[index, f'{model_name}_caption_with_orig_caption'] = only_caption
 
         if index < 10:
-            save_generation_info(index, prompt1, prompt2, orig_caption, caption, caption_with_orig_caption, model_name)                
+            save_generation_info(index, prompt1, prompt2, orig_caption, caption, caption_with_orig_caption, model_name, row['file_name'])
 
         if index % 20 == 0:
             df.to_json(metadata_file_path, orient='records', lines=True)
@@ -108,6 +112,12 @@ def generate_images_captions(df, model, processor, model_prompt, model_eop, mode
 if __name__ == "__main__":
     metadata_df = pd.read_json(path_or_buf=metadata_file_path, lines=True)
 
+    # remove the columns - used for regeneration of captions    
+    metadata_df = metadata_df.drop(['llava_caption_with_orig_caption', 'llava_next_caption_with_orig_caption'], axis=1, errors='ignore')
+
+    # sample 100
+    # metadata_df = metadata_df.sample(n=100, ignore_index=True)
+    
     quantization_config = get_quantization_config()
 
     print("generating captions with llava")
@@ -116,6 +126,6 @@ if __name__ == "__main__":
 
     del llava_model, llava_processor, llava_model_prompt, llava_model_eop
 
-    print("\ngenerating captions with llava next")
-    llava_next_model, llava_next_processor, llava_next_prompt, llava_next_model_eop = get_llava_next_model_and_processor(quantization_config)
-    generate_images_captions(metadata_df, llava_next_model, llava_next_processor, llava_next_prompt, llava_next_model_eop, 'llava_next')
+    # print("\ngenerating captions with llava next")
+    # llava_next_model, llava_next_processor, llava_next_prompt, llava_next_model_eop = get_llava_next_model_and_processor(quantization_config)
+    # generate_images_captions(metadata_df, llava_next_model, llava_next_processor, llava_next_prompt, llava_next_model_eop, 'llava_next')
