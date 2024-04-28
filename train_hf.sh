8a95e81409c44b03aa9ddf3695b1b3ad04f74783
raulc <raulciotescu@gmail.com>
2024-04-28 18:09:01 +0200

    400 train samples

--- train_hf.sh
+++ train_hf.sh
@@ -23,6 +23,7 @@ accelerate launch --num_processes=1 --main_process_port=36667 PixArt-alpha/train
   --resolution=512 \
   --train_batch_size=2 --gradient_accumulation_steps=1 \
   --num_train_epochs=100 --checkpointing_steps=100 \
+  --max_train_samples=400 \
   --learning_rate=3e-04 --lr_scheduler="cosine" --lr_warmup_steps=0 \
   --seed=42 \
   --output_dir="pixart-simpsons-model" \
@@ -33,8 +34,7 @@ accelerate launch --num_processes=1 --main_process_port=36667 PixArt-alpha/train
   --rank=16 \
   --adam_weight_decay=0.03 --adam_epsilon=1e-10 \
   --dataloader_num_workers=8
-  # --max_train_samples=400
-  # --snr_gamma=1.0
+    # --snr_gamma=1.0
   # --use_rslora
   # --use_dora
 
