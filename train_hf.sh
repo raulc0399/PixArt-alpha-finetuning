c958a616db42cacdefc1fcb4d7c233eea7fea9c6
raci0399 <raci0399@gmail.com>
2024-05-13 12:24:35 +0200

    prepare for lego city training

--- train_hf.sh
+++ train_hf.sh
@@ -13,22 +13,22 @@
 # validation will be ran at the end
 accelerate launch --num_processes=1 --main_process_port=36667 PixArt-alpha/train_scripts/train_pixart_lora_hf.py --mixed_precision="bf16" \
   --pretrained_model_name_or_path=PixArt-alpha/PixArt-XL-2-512x512 \
-  --train_data_dir="../data/train/" --caption_column="llava_caption_with_orig_caption" \
+  --train_data_dir="../data/lego-city-adventures-captions/" --caption_column="llava_caption_with_orig_caption" \
   --resolution=512 \
   --train_batch_size=2 --gradient_accumulation_steps=1 \
-  --num_train_epochs=100 --checkpointing_steps=500 \
-  --max_train_samples=300 \
+  --num_train_epochs=100 --checkpointing_steps=200 \
   --checkpoints_total_limit=30 \
   --learning_rate=3e-04 --lr_scheduler="cosine" --lr_warmup_steps=0 \
   --seed=42 \
-  --output_dir="pixart-simpsons-model" \
+  --output_dir="lego-city-adventures-model" \
   --report_to="wandb" \
   --gradient_checkpointing \
   --validation_epochs=5 \
-  --validation_prompt="Image in the style of simpsons cartoons, cute dragon creature" \
+  --validation_prompt="Image in lego city adventures style, cute dragon creature" \
   --rank=16 \
   --adam_weight_decay=0.03 --adam_epsilon=1e-10 \
   --dataloader_num_workers=8
+  # --max_train_samples=300 \
   # --train_text_encoder --text_encoder_learning_rate=5e-05 \
   # --text_encoder_stop_at_percentage_steps=0.2 \
   # --snr_gamma=1.0
